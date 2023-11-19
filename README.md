b34989e15e41c85b8f340c1bd26d928749eaa589
farisabimanyu <abimanyuputraarjuna2708@gmail.com>
2023-11-19 23:06:29 +0700

    rampung

---
+++ README.md
@@ -0,0 +1,15 @@
+<h1>Hasil</h1>
+<h2>Home Page<br></h2>
+<img src="tampilan awal.png"><br><br>
+<h2>Tambah Data Page<br></h2>
+<img src="tambah_data1.png"><br><br>
+<img src="tambah_data2.png"><br><br>
+<img src="tambah_data3.png"><br><br>
+<h2>Tampilan After Tambah Data<br></h2>
+<img src="tampilan after tambah data.png"><br><br>
+<h2>Edit Data Page<br></h2>
+<img src="edit data.png"><br><br>
+<h2>Tampilan After Edit Data<br></h2>
+<img src="tampilan after edit data.png"><br><br>
+<h2>Tampilan After Delete Data Draco Malfoy<br></h2>
+<img src="delete data.png"><br><br>
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'mahasiswa',
     pathMatch: 'full',
+  },
+  // {
+  //   path: 'folder/:id',
+  //   loadComponent: () =>
+  //     import('./folder/folder.page').then((m) => m.FolderPage),
+  // },
+  {
+    path: 'mahasiswa',
+    loadComponent: () => import('./mahasiswa/mahasiswa.page').then(m => m.MahasiswaPage)
   },
 ];
